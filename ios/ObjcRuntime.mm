1bd885bc3e1632e5256afd7de25e272acbc9bc4a
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2021-10-15 12:47:50 +0100

    chore: move all objc++ files out of the cpp dir

--- ios/ObjcRuntime.mm
+++ ios/ObjcRuntime.mm
@@ -1,7 +1,10 @@
 #import <React/RCTBridge+Private.h>
 #import <React/RCTConvert.h>
 #import "ObjcRuntime.h"
-#import "../cpp/react-native-objc-runtime.h"
+#import "react-native-objc-runtime.h"
+
+// This is the entrypoint file for our native module.
+// It will be called by React Native as it initialises its native modules.
 
 @implementation ObjcRuntime
 
@@ -20,6 +23,7 @@
     return;
   }
   
+  // Install the `objc` HostObject into the JS runtime in global scope.
   ObjcRuntimeJsi::install(*(facebook::jsi::Runtime *)cxxBridge.runtime);
 }
 
@@ -31,6 +35,8 @@
   if (!cxxBridge.runtime) {
     return;
   }
+  
+  // Overwrite the `objc` HostObject as undefined (my speculative way of cleaning it up).
   ObjcRuntimeJsi::uninstall(*(facebook::jsi::Runtime *)cxxBridge.runtime);
 }
 
