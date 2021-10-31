be3fa00912bbda8896773537ef1bea048285cc16
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2021-10-31 09:55:39 +0000

    fix: don't call objc runtime in Android apps

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,78 +1,10 @@
 /// <reference path="objc-types.d.ts" />
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Platform } from 'react-native';
 
 export default function App() {
   React.useEffect(() => {
-    const hello: objc.NSString =
-      objc.NSString.alloc()['initWithString:']('Hello');
-    const helloWorld: objc.NSString =
-      hello['stringByAppendingString:'](', world!');
-    console.log('Concatenate two NSStrings:', helloWorld.toJS());
-
-    console.log(
-      `Marshal UTF-8 text back and forth, given "白樺":`,
-      objc.NSString.alloc()['initWithString:']('白樺').toJS()
-    );
-
-    console.log(
-      `Get unicode name for each character, given "🐝":`,
-      objc.NSString.alloc()
-        ['initWithString:']('🐝')
-        ['stringByApplyingTransform:reverse:']('Name-Any', false)
-        .toJS()
-    );
-
-    // Fun with Foundation String Transforms!
-    // @see https://nshipster.com/ios9/
-    // @see https://nshipster.com/cfstringtransform/
-    // @see https://sites.google.com/site/icuprojectuserguide/transforms/general#TOC-ICU-Transliterators
-    // @see https://twitter.com/LinguaBrowse/status/1390225265612181505?s=20
-    console.log(
-      'Convert Chinese script from Trad. -> Simp., given "漢字簡化爭論":',
-      objc.NSString.alloc()
-        ['initWithString:']('漢字簡化爭論')
-        ['stringByApplyingTransform:reverse:']('Simplified-Traditional', false)
-        .toJS()
-    );
-
-    console.log(
-      'Look up the global variable "NSStringTransformLatinToHiragana" in order to transliterate Japanese Hiragana to Latin, given "しらかば":',
-      objc.NSString.alloc()
-        ['initWithString:']('しらかば')
-        ['stringByApplyingTransform:reverse:'](
-          (objc as any).NSStringTransformLatinToHiragana,
-          false
-        )
-        .toJS()
-    );
-
-    console.log(
-      'Do the same, this time using the equivalent Core Foundation symbol, "kCFStringTransformToLatin":',
-      objc.NSString.alloc()
-        ['initWithString:']('しらかば')
-        ['stringByApplyingTransform:reverse:'](
-          (objc as any).kCFStringTransformToLatin,
-          false
-        )
-        .toJS()
-    );
-
-    console.log(
-      'Transliterate Korean Hangul to Latin, given "안녕하세요":',
-      objc.NSString.alloc()
-        ['initWithString:']('안녕하세요')
-        ['stringByApplyingTransform:reverse:']('Latin-Hangul', false)
-        .toJS()
-    );
-
-    /* Uncomment this if you're happy to hear a voice speak! */
-    // const utterance =
-    //   objc.AVSpeechUtterance.alloc()['initWithString:']('Hello, world!');
-    // utterance.voice =
-    //   objc.AVSpeechSynthesisVoice['voiceWithLanguage:']('en-GB');
-    // objc.AVSpeechSynthesizer.alloc().init()['speakUtterance:'](utterance);
+    Platform.OS === 'android' ? javaExperiments() : objcExperiments();
   }, []);
 
   return (
@@ -80,6 +12,81 @@ export default function App() {
       <Text>Placeholder text</Text>
     </View>
   );
+}
+
+function javaExperiments(): void {
+  // TODO
+}
+
+function objcExperiments(): void {
+  const hello: objc.NSString =
+    objc.NSString.alloc()['initWithString:']('Hello');
+  const helloWorld: objc.NSString =
+    hello['stringByAppendingString:'](', world!');
+  console.log('Concatenate two NSStrings:', helloWorld.toJS());
+
+  console.log(
+    `Marshal UTF-8 text back and forth, given "白樺":`,
+    objc.NSString.alloc()['initWithString:']('白樺').toJS()
+  );
+
+  console.log(
+    `Get unicode name for each character, given "🐝":`,
+    objc.NSString.alloc()
+      ['initWithString:']('🐝')
+      ['stringByApplyingTransform:reverse:']('Name-Any', false)
+      .toJS()
+  );
+
+  // Fun with Foundation String Transforms!
+  // @see https://nshipster.com/ios9/
+  // @see https://nshipster.com/cfstringtransform/
+  // @see https://sites.google.com/site/icuprojectuserguide/transforms/general#TOC-ICU-Transliterators
+  // @see https://twitter.com/LinguaBrowse/status/1390225265612181505?s=20
+  console.log(
+    'Convert Chinese script from Trad. -> Simp., given "漢字簡化爭論":',
+    objc.NSString.alloc()
+      ['initWithString:']('漢字簡化爭論')
+      ['stringByApplyingTransform:reverse:']('Simplified-Traditional', false)
+      .toJS()
+  );
+
+  console.log(
+    'Look up the global variable "NSStringTransformLatinToHiragana" in order to transliterate Japanese Hiragana to Latin, given "しらかば":',
+    objc.NSString.alloc()
+      ['initWithString:']('しらかば')
+      ['stringByApplyingTransform:reverse:'](
+        (objc as any).NSStringTransformLatinToHiragana,
+        false
+      )
+      .toJS()
+  );
+
+  console.log(
+    'Do the same, this time using the equivalent Core Foundation symbol, "kCFStringTransformToLatin":',
+    objc.NSString.alloc()
+      ['initWithString:']('しらかば')
+      ['stringByApplyingTransform:reverse:'](
+        (objc as any).kCFStringTransformToLatin,
+        false
+      )
+      .toJS()
+  );
+
+  console.log(
+    'Transliterate Korean Hangul to Latin, given "안녕하세요":',
+    objc.NSString.alloc()
+      ['initWithString:']('안녕하세요')
+      ['stringByApplyingTransform:reverse:']('Latin-Hangul', false)
+      .toJS()
+  );
+
+  /* Uncomment this if you're happy to hear a voice speak! */
+  // const utterance =
+  //   objc.AVSpeechUtterance.alloc()['initWithString:']('Hello, world!');
+  // utterance.voice =
+  //   objc.AVSpeechSynthesisVoice['voiceWithLanguage:']('en-GB');
+  // objc.AVSpeechSynthesizer.alloc().init()['speakUtterance:'](utterance);
 }
 
 const styles = StyleSheet.create({
